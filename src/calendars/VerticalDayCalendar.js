b2c80b76a0472d8c015eca6505a2463b1f322d51
Karl MUSINGO <karlemusingo@gmail.com>
2020-01-14 11:55:51 +0200

    add vertical day calendar

--- src/calendars/VerticalDayCalendar.js
+++ src/calendars/VerticalDayCalendar.js
@@ -10,9 +10,9 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 import DisplayText from './DisplayText';
 import Event from './Event';
-import { weekdays, months, cellWidthDayView } from './Constants';
+import { weekdays, months , cellHeightVerticalDayView } from './Constants';
 import compareDate from '../utils/compareDate';
-import '../styles/dayCalendar.scss';
+import '../styles/verticalDayCalendar.scss';
 
 const today = new Date();
 
@@ -36,14 +36,20 @@ export const addSuffix = (date) => {
 };
 
 /* istanbul ignore next */
-const DayCalendar = ({ events = [], dayOrientation }) => {
+const DayCalendar = ({ events = [] }) => {
   const [day, setDay] = useState(today);
 
   const calendarBody = useRef(null);
 
   useEffect(() => {
+    let i = 0;
+    const allEvents = document.querySelectorAll('.event');
+    for (i = 0; i < allEvents.length; i++) {
+      allEvents[i].style.width = `${98 / allEvents.length}%`;
+    }
     startOn(today.getHours());
   }, [day]);
+
 
   const formatHours = (hour) => {
     if (hour > 12) return `${hour - 12}PM`;
@@ -52,8 +58,8 @@ const DayCalendar = ({ events = [], dayOrientation }) => {
   };
   const startOn = (hour) => {
     calendarBody.current.scrollBy({
-      top: 0,
-      left: hour * cellWidthDayView,
+      top: hour * cellHeightVerticalDayView,
+      left: 0,
     });
   };
 
@@ -82,7 +88,7 @@ const DayCalendar = ({ events = [], dayOrientation }) => {
   const history = [];
 
   return (
-    <div id="day-calendar">
+    <div id="vertical-day-calendar">
       <div className="calendar-header">
         <div className="month" style={{ marginLeft: '196px' }}>
           <ul>
@@ -109,57 +115,61 @@ const DayCalendar = ({ events = [], dayOrientation }) => {
         </div>
       </div>
       <div className="calendar-body" ref={calendarBody}>
-        <ul id="day-time">
+        {/* <ul id="day-time">
           {
-           [...(Array(24))].map((el, index) => (
+           [...(Array(2))].map((el, index) => (
              <li key={Math.random()}>
                <DisplayText text={formatHours(index)} />
              </li>
            ))
-         }
-        </ul>
+          }
+        </ul> */}
 
         <ul id="days">
+          {[...(Array(24 * 2))].map((value, index) => {
+            if(index % 2 === 0) return (<li className="hour" key={Math.random()}>
+                <DisplayText text={formatHours(index/2)} />
+              </li>)
+            return (<li className="events-cells" style={{}} key={Math.random()}>
+              {
+                events.map((event) => {
+                  if (!compareDate(event.startTime, day)) return null;
+                  event.startTime = new Date(event.startTime);
+                  const time = event.startTime.getHours();
+                  if(index !== (time * 2) + 1) return null;
+                  const minutPush = event.startTime.getMinutes() * (cellHeightVerticalDayView / 60);
+                  const duration = ((event.endTime.getTime() - event.startTime.getTime())
+                        / 60000) * (cellHeightVerticalDayView / 60);
+                  const exist = history.filter(el =>
+                                  el.time === time
+                                  ||
+                                  (
+                                  (el.time * cellHeightVerticalDayView + el.minutPush)
+                                  <
+                                  (time * cellHeightVerticalDayView + minutPush)
+                                  &&
+                                  (time * cellHeightVerticalDayView + minutPush)
+                                  <
+                                  (el.time * cellHeightVerticalDayView + el.minutPush + el.duration)),
+                  );
 
-          {
-              events.map((event) => {
-                if (!compareDate(event.startTime, day)) return null;
-                event.startTime = new Date(event.startTime);
-                const time = event.startTime.getHours();
-                const minutPush = event.startTime.getMinutes() * (cellWidthDayView / 60);
-                const duration = ((event.endTime.getTime() - event.startTime.getTime())
-                / 60000) * (cellWidthDayView / 60);
-                const exist = history.filter(el =>
-                                 el.time === time
-                                 ||
-                                 (
-                                 (el.time * cellWidthDayView + el.minutPush)
-                                 <
-                                 (time * cellWidthDayView + minutPush)
-                                 &&
-                                 (time * cellWidthDayView + minutPush)
-                                 <
-                                 (el.time * cellWidthDayView + el.minutPush + el.duration)),
-               );
-
-                history.push({ time, duration, minutPush });
-                return (
-                  <Event
-                    key={Math.random()}
-                    style={{
-                      left: `${time * cellWidthDayView}px`,
-                      top: `${exist.length * 32}px`,
-                      marginLeft: `${minutPush}px`,
-                      width: `${duration}px`,
-                    }}
-                    title={event.title}
-                  />
-                );
-              })
-           }
-          {[...(Array(24 * 18))].map((value, index) => (
-            <li style={{}} key={Math.random()} />
-           ))}
+                  history.push({ time, duration, minutPush });
+                  return  (
+                    <Event
+                      key={Math.random()}
+                      style={{
+                        height: `${duration}px`,
+                        // top: `${time * cellHeightVerticalDayView}px`,
+                        marginTop: `${minutPush}px`,
+                        // width: `${95 / exist.length}%`,
+                      }}
+                      title={event.title}
+                    />
+                  );
+                })
+              }
+            </li>)
+          })}
         </ul>
       </div>
 
@@ -170,9 +180,10 @@ const DayCalendar = ({ events = [], dayOrientation }) => {
 DayCalendar.propTypes = {
   events: PropTypes.arrayOf(
     PropTypes.shape({
-      startTime: PropTypes.object,
-      fontSize: PropTypes.number,
-    }),
+      title: PropTypes.string,
+      startTime: PropTypes.instanceOf(Date),
+      endTime: PropTypes.instanceOf(Date)
+    })
   ),
 };
 
---
+++ src/utils/compareDate.js
@@ -0,0 +1,5 @@
+const compareDate = (date1, date2) => {
+    return date1.getDate() === date2.getDate() && date1.getMonth() === date2.getMonth() &&date1.getYear() === date2.getYear() ;
+}
+
+export default compareDate;
