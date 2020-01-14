3eb32869d1257cacb0ff69b9f13258d0558fc750
Emmanuel Rukundo <emmanuelrukundo@Emmanuels-MacBook-Pro-3.local>
2020-01-14 23:20:18 +0200

    story: add hours vertically on week calendar

--- src/utils/useCalendarDay.js
+++ src/utils/useCalendarDay.js
@@ -1,7 +1,5 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 
-const useCalendarDay = (defaultDay = new Date()) => {
-  return useState(defaultDay);
-}
-
+const useCalendarDay = (defaultDay = new Date()) => useState(defaultDay);
 export default useCalendarDay;
