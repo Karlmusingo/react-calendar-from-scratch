56fc56fed45f511688a9066802da99bf6c56a2ac
Emmanuel Rukundo <emmanuelrukundo@Emmanuels-MacBook-Pro-3.local>
2020-01-15 11:46:54 +0200

    display events on the calendar

--- src/calendars/Event.js
+++ src/calendars/Event.js
@@ -1,9 +1,10 @@
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
 import DisplayText from './DisplayText';
 
 const Event = ({ ttitle, title, style }) => (
-  <div className="event" style={style} >
+  <div className="event" style={style}>
     <DisplayText text={ttitle} name={title} />
   </div>
 );
