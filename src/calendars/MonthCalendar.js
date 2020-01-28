c1446ec3ef09101ee3722b4956f0a3a0764c2145
Emmanuel Rukundo <emmanuelrukundo@Emmanuels-MacBook-Pro-3.local>
2020-01-28 15:54:34 +0200

    calendars-have-same-dimensions

--- src/calendars/MonthCalendar.js
+++ src/calendars/MonthCalendar.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable array-callback-return */
@@ -32,7 +33,7 @@ const MonthCalendar = ({ events, onMonthChange }) => {
     const newMonth = month === 0 ? 11 : month - 1;
     setMonth(newMonth);
     setyear(newYear);
-    onMonthChange(newMonth, newYear)
+    onMonthChange(newMonth, newYear);
   };
 
   return (
@@ -58,26 +59,33 @@ const MonthCalendar = ({ events, onMonthChange }) => {
       </ul>
 
       <ul id="days">
-       {
+        {
           [
             ...(Array(skipDays)).fill('*'),
             ...Array(numberOfDays).keys(),
-            ...Array(42 - (skipDays + numberOfDays)).keys()
+            ...Array(42 - (skipDays + numberOfDays)).keys(),
           ].map((day, index) => (
-            day === '*' ? 
-            <li className="empty-cells" key={Math.random()}> { numberOfDaysPreviousMonth - (skipDays - 1) + index } </li> 
-            :
-            <li className={ index > (numberOfDays + skipDays - 1) ? 'empty-cells' : " "} key={Math.random()}>
-               {
-                  day + 1 && day + 1 === today.getDate() ? (
+            day === '*'
+              ? (
+                <li className="empty-cells" key={Math.random()}>
+                  {' '}
+                  { numberOfDaysPreviousMonth - (skipDays - 1) + index }
+                  {' '}
+                </li>
+              )
+              : (
+                <li className={index > (numberOfDays + skipDays - 1) ? 'empty-cells' : ' '} key={Math.random()}>
+                  {
+                  day + 1 && day + 1 === today.getDate() && today.getMonth() === month
+                  && yearDefault === year ? (
                     <span className="currentDay">
                       {day + 1}
                     </span>
-                  ) : day + 1
+                    ) : day + 1
               }
              &nbsp;
-                <br />
-                {
+                  <br />
+                  {
                   events.map((event) => {
                     if (day + 1 === event.startTime.getDate()
                      && month === event.startTime.getMonth()) {
@@ -92,9 +100,9 @@ const MonthCalendar = ({ events, onMonthChange }) => {
                     }
                   })
                 }
-              </li>
-            )
-          )
+                </li>
+              )
+          ))
         }
 
       </ul>
