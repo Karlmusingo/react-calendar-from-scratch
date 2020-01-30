d11b8c898182b0334ce1350e426deedf842cd724
Karl MUSINGO <karlemusingo@gmail.com>
2020-01-30 13:04:00 +0200

    Updates

---
+++ precache-manifest.3ce1143f9ba0126c539066ed61ca009e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0cc96907c16a2d4214df9af918a98e",
+    "url": "/react-calendar-from-scratch/index.html"
+  },
+  {
+    "revision": "bfb9eaf6d0df5b749dfe",
+    "url": "/react-calendar-from-scratch/static/css/main.da9c86c1.chunk.css"
+  },
+  {
+    "revision": "792f0ba3efc040ed449c",
+    "url": "/react-calendar-from-scratch/static/js/2.42390c4d.chunk.js"
+  },
+  {
+    "revision": "bfb9eaf6d0df5b749dfe",
+    "url": "/react-calendar-from-scratch/static/js/main.2eae58d1.chunk.js"
+  },
+  {
+    "revision": "79ba9dac1f32210d3dc9",
+    "url": "/react-calendar-from-scratch/static/js/runtime-main.59d6a01f.js"
+  }
+]);
